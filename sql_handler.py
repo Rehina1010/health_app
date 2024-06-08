96c34f52ab61fa0c1fd87ddccb21dd9e34cf2661
Regina0101 <ritodo1010@gmail.com>
2024-06-08 14:24:00 +0100

    create tables

---
+++ sql_handler.py
@@ -0,0 +1,19 @@
+from connection import create_connection, database
+
+def create_table(conn, sql_path):
+    with open(sql_path) as file:
+        sql_expr = file.read()
+        cur = conn.cursor()
+        cur.executescript(sql_expr)
+        conn.commit()
+
+
+
+
+def main():
+    with create_connection(database) as conn:
+        create_table(conn, 'sql_files/create_tables.sql')
+
+
+if __name__ == "__main__":
+    main()
